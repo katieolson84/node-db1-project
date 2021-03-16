1f055334f0ae15624f776832f2285abc463275c4
katieolson84 <katieolson84@gmail.com>
2021-03-15 19:27:03 -0600

    Delete and Put are working

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,26 +1,60 @@
 const Account = require('../accounts/accounts-model')
 
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountPayload = async (req, res, next) => {
+  try{
+    const name = req.body.name;
+    const budget = req.body.budget;
+
+    if(!name || !budget) {
+      res.status(400).json({message: "name and budget are required"})
+    }else if (typeof name !== 'string') {
+      res.status(400).json({message: "name of account must be a string"})
+    }else if (typeof budget !== 'number'){
+      res.status(400).json({message: "budget of account must be a number"})
+    }else if (name.trim().length < 3 || name.trim().length > 100) {
+      res.status(400).json({message: "name of account must be between 3 and 100"})
+    }else if (budget < 0 || budget > 1000000) {
+      res.status(400).json({message: "budget of account is too large or too small"})
+    }else {
+      req.body.name = name.trim()
+      next()
+    }
+  }catch {
+    next();
+  }
 }
 
-// exports.checkAccountNameUnique = async (req, res, next) => {
-//   if(!req.body)
-  
-// }
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try{
+    const existingAccounts = await Account.getAll()
+    const name = req.body.name.trim()
+    const checkName = existingAccounts.filter((item) => {
+      if(item.name === name) {
+        res.json(name)
+      }
+    })
+    if( checkName.length > 0) {
+      res.status(400).json({message: "that name is taken"})
+    }else {
+      next()
+    }
+  }catch (err) {
+    next(err)
+  }
+}
 
 exports.checkAccountId = async (req, res, next) => {
   try{
-    const account = await Account.getAll(req.params.id)
-    if(!account) {
-      res.status(404).json({
-        message: "account not found"
-      })
+    const account = await Account.getById(req.params.id)
+    if(account) {
+     req.account = account
+     next()
     }else{
-      req.account = account
-      next()
+      res.status(404).json({message: "account not found"})
     }
   }catch(err) {
     next(err)
   }
 };
+
+
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const Account = require('./accounts-model')
-const {checkAccountId} = require('./accounts-middleware');
+const {checkAccountId, checkAccountPayload, checkAccountNameUnique} = require('./accounts-middleware');
 
 router.get('/', async (req, res, next) => {
   try{
@@ -20,16 +20,32 @@ router.get('/:id', checkAccountId, async (req, res, next) => {
   }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountNameUnique, checkAccountPayload, async (req, res, next) => {
+  try {
+    const data = await Account.create(req.body)
+    res.status(201).json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+
+router.put('/:id', checkAccountPayload, checkAccountId, async (req, res, next) => {
+  try{
+    const data = await Account.updateById(req.params.id, req.body)
+    res.json(data)
+  }catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', checkAccountId, async (req, res, next) => {
+  try{
+    const data = await Account.deleteById(req.params.id)
+    res.json(data)
+  }catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
